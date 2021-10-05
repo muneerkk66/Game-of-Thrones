79967980e2c7daeffb9d13e1460216177dda9d12
Muneer KK <k.muneer@tcs.com>
2021-10-05 21:57:14 +0530

    Implement unit test for api call

--- iOSTakeHomeChallenge/iOSTakeHomeChallengeTests/iOSTakeHomeChallengeTests.swift
+++ iOSTakeHomeChallenge/iOSTakeHomeChallengeTests/iOSTakeHomeChallengeTests.swift
@@ -6,27 +6,31 @@
 //
 
 import XCTest
+@testable import iOSTakeHomeChallenge
 
 class iOSTakeHomeChallengeTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var mockUrlSession: MockAPISession!
+    var network: NetWorkManager!
+    
+    struct SomeIntResponse: Decodable {
+        let someInt: Int
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func setUp() {
+        mockUrlSession = MockAPISession()
+        network = NetWorkManager()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testDeserialiseBookData() throws {
+        mockUrlSession.completeWithResponse = HTTPURLResponse(url: URL(fileURLWithPath: ""), statusCode: 200, httpVersion: nil, headerFields: nil)
+        mockUrlSession.completeWithData = try JSONSerialization.data(withJSONObject: ["someInt": 120])
+        
+        network.makeRequest(url: URL(string: APIConstants.baseURL.appending(APIConstants.APIUrls.books.rawValue))!, type: [Book].self, completionHandler: { error, books in
+            if let books = books {
+                assert((books as Any) is [Book])
+            }
+        })
     }
 
 }
