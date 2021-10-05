e3a398e7566949123ff88b0ebe382f40cbda0fbe
Muneer KK <k.muneer@tcs.com>
2021-10-05 19:33:27 +0530

    Integrate network manager

--- iOSTakeHomeChallenge/iOSTakeHomeChallenge/NetworkManager/NetWorkManager.swift
+++ iOSTakeHomeChallenge/iOSTakeHomeChallenge/NetworkManager/NetWorkManager.swift
@@ -6,3 +6,67 @@
 //
 
 import Foundation
+
+enum NetworkError: Error {
+    case connection
+    case dataEmpty
+    case general
+    case jsonDecode
+    case serverError
+}
+
+class NetWorkManager{
+    
+    let baseURL: String = "https://anapioficeandfire.com/api/"
+    
+    func makeRequest<T: Decodable>(url: URL, type: T.Type, completionHandler: @escaping (_ error: Error?, _ myObject: T?) -> ()) {
+        
+        guard Reachability.isConnectedToNetwork() else {
+            self.handleError(error: nil, type: .connection)
+            return
+        }
+
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            guard error == nil else {
+                self.handleError(error: error?.localizedDescription, type: .general)
+                return
+            }
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                if !(200...299).contains(httpResponse.statusCode) {
+                    self.handleError(error: "Server Error", type: .serverError, code: httpResponse.statusCode)
+                }
+            }
+            
+            guard let data = data else {
+                self.handleError(type: .dataEmpty)
+                return
+            }
+            
+            do {
+                let myNewObject = try JSONDecoder().decode(T.self, from: data)
+                completionHandler(nil, myNewObject)
+            } catch let error {
+                completionHandler(error, nil)
+                return
+            }
+        }.resume()
+    }
+    
+    private func handleError(error: String? = nil, type: NetworkError, code: Int? = nil) {
+    
+        switch type {
+        case .connection:
+            print("Connection Error!, Unable to connect to the internet.")
+        case .general:
+            print("Error: \(error ?? "Unknown")")
+        case .dataEmpty:
+            print("Error! Empty Data")
+        case .serverError:
+            if let error = error {print(error.appending(String(code ?? 0)))}
+        case .jsonDecode:
+            print("JSON Decoding Error!, Unable to Decode JSON.")
+        }
+    }
+}
--- iOSTakeHomeChallenge/iOSTakeHomeChallenge/Utils/AppConstants.swift
+++ iOSTakeHomeChallenge/iOSTakeHomeChallenge/Utils/AppConstants.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+class AppConstants: NSObject {
+    
+    enum BaseURL: String {
+        case production = "https://anapioficeandfire.com"
+        case test = "https://anapioficeandfiretest.com"
+    }
+    
+    // MARK: - Pointed Server
+    static let baseURL = BaseURL.production.rawValue
+    
+    
+    enum APIUrls: String {
+        case signUp = "/api/books"
+        case login = "/api/houses"
+        case search = "/api/characters"
+    }
+}
